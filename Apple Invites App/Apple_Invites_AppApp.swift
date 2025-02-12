7b2d1952e606b9df67188de323efe9c47d948eb8
rick2785 <rjhrabowskie@gmail.com>
2025-02-12 11:39:25 -0600

    Initial Commit

---
+++ Apple Invites App/Apple_Invites_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Invites_AppApp.swift
+//  Apple Invites App
+//
+//  Created by RJ Hrabowskie on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Invites_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
